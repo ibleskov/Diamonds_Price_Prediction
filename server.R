504f8c7314d2fe1b2c5e71d92905aedba9d5cdfa
Dr. Ivan Bleskov <ivan.bleskov@gmail.com>
2017-03-04 14:02:49 +0100

    Edit server.R

--- server.R
+++ server.R
@@ -18,21 +18,27 @@ shinyServer(function(input, output) {
                                                   clarity = input$clarity))
 
         g <- ggplot(data = d, aes(x = carat, y = price)) +
-            geom_point(alpha = .3)+
-            geom_smooth(method = "lm", formula = y ~ x + I(x^2)) +
-            geom_vline(xintercept = input$carat, color = "blue") +
-            geom_hline(yintercept = pred, color = "blue")
+             geom_point(alpha = .3) +
+             geom_smooth(method = "lm", formula = y ~ x + I(x^2)) +
+             geom_vline(xintercept = input$carat, color = "blue") +
+             geom_hline(yintercept = pred, color = "blue")
         g
     })
 
     output$result <- renderText({
-        d <- filter(diamonds, grepl(input$cut, cut), grepl(input$color, color), grepl(input$clarity, clarity))
-        fit <- lm(price ~ carat + I(carat^2), d)
-        pred <- predict(fit, newdata = data.frame(carat = input$carat,
-                                                  cut = input$cut,
-                                                  color = input$color,
+        d <- diamonds %>%
+            filter(grepl(input$cut,     cut), 
+                   grepl(input$color,   color), 
+                   grepl(input$clarity, clarity))
+        
+	fit <- lm(price ~ carat + I(carat^2), d)
+        
+	pred <- predict(fit, newdata = data.frame(carat   = input$carat,
+                                                  cut     = input$cut,
+                                                  color   = input$color,
                                                   clarity = input$clarity))
-        res <- paste0("You can gain about $", round(pred, 0), " for that one!")
+        
+	res <- paste0("You can gain about $", round(pred, 0), " for that one!")
         res
     })
     
